049808e371de89c550a3d3a7186b83ff223fb12a
erniputri <erni22si@mahasiswa.pcr.ac.id>
2025-12-30 21:48:11 +0700

    coba deploy

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,10 +1,6 @@
 <?php
-
 namespace App\Console;
 
-use App\Console\cron\ReminderDinner;
-use App\Console\cron\ReminderLunch;
-use App\Console\cron\SendNotificationWa;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,17 +10,17 @@ class Kernel extends ConsoleKernel
      * Define the application's command schedule.
      */
     protected function schedule(\Illuminate\Console\Scheduling\Schedule $schedule)
-{
-    $schedule->command('wa:send-notification')
-             ->everyMinute();
-}
+    {
+        $schedule->command('wa:send-notification')
+            ->everyMinute();
+    }
 
     /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
