c5eee51367b7b3450fa72edc6360dcf2de536d44
Erni <erni22si@mahasiswa.pcr.ac.id>
2026-01-13 15:14:30 +0700

    perubahan logo

--- resources/views/partials/css.blade.php
+++ resources/views/partials/css.blade.php
@@ -1,18 +1,12 @@
 <link rel="stylesheet" href="{{ asset('assets/vendors/feather/feather.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/vendors/ti-icons/css/themify-icons.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}">
-<!-- endinject -->
 
-<!-- Plugin css for this page -->
 <link rel="stylesheet" href="{{ asset('assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/vendors/ti-icons/css/themify-icons.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('js/select.dataTables.min.css') }}">
-<!-- End plugin css for this page -->
 
-<!-- inject:css -->
 <link rel="stylesheet" href="{{ asset('assets/css/vertical-layout-light/style.css') }}">
-<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
-
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}">
 
 <style>
     .search-input {
@@ -25,5 +19,47 @@
         border-radius: 8px;
         height: 38px;
         font-weight: 500;
+    }
+
+    .sidebar-header-horizontal {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        padding: 18px 15px;
+        border-bottom: 1px solid #e4e4e4;
+
+    }
+
+    .sidebar-logo-horizontal {
+        width: 38px;
+    }
+
+    .sidebar-title-horizontal {
+        font-size: 18px;
+        font-weight: 600;
+        color: #2f7a3e;
+    }
+
+    .navbar-brand-text {
+        font-size: 18px;
+        font-weight: 600;
+        color: #2f7a3e;
+        white-space: nowrap;
+    }
+
+    .sidebar-menu-header {
+        margin: 12px 15px 8px;
+        padding: 8px 12px;
+        background-color: #f5f6fa;
+        color: #6c757d;
+        font-size: 12px;
+        font-weight: 900;
+        text-transform: uppercase;
+        border-radius: 6px;
+        letter-spacing: 0.6px;
+    }
+
+    .sidebar .nav {
+        margin-top: 0;
     }
 </style>
