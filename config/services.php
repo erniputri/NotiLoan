ddca7b704a6bdd3988ec21ea60ecf6597dcd3af3
erniputri <erni22si@mahasiswa.pcr.ac.id>
2026-04-19 22:30:24 +0700

    penambahan letak api whatsap

--- config/services.php
+++ config/services.php
@@ -18,21 +18,27 @@ return [
         'key' => env('POSTMARK_API_KEY'),
     ],
 
-    'resend' => [
+    'resend'   => [
         'key' => env('RESEND_API_KEY'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
+    'ses'      => [
+        'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'slack' => [
+    'slack'    => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'channel'              => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+
+    // 'whatsapp' => [
+    //     'url'       => env('WA_API_URL'),
+    //     'token'     => env('WA_API_TOKEN'),
+    //     'device_id' => env('WA_API_DEVICE_ID'),
+    // ],
 
 ];
